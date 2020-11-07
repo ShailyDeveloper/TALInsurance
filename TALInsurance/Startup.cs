ed4f194c77dba5fc60f93015d5a333539355f6b6
ShailyDeveloper <62253565+ShailyDeveloper@users.noreply.github.com>
2020-11-07 15:43:33 +1100

    added cors policy

--- TALInsurance/Startup.cs
+++ TALInsurance/Startup.cs
@@ -46,6 +46,7 @@ namespace TALInsurance
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
